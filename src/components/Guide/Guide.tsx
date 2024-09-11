429799e48a9f87083dee63d01434d6f0c55a6bcf
jasnmar <jasnmar@gmail.com>
2024-09-11 14:10:42 -0400

    Updated Guides List

--- src/components/Guide/Guide.tsx
+++ src/components/Guide/Guide.tsx
@@ -7,24 +7,22 @@ import {
   instLogo,
   youTubeLogo,
   linkedInLogo,
-  emailLogo } from "../../assets/images"
+  emailLogo,
+  venmoLogo } from "../../assets/images"
 import { guideDataInterface } from "../../pages/Guides/GuideInterfaces"
 import "./Guide.css"
 
-
 function Guide( props : guideDataInterface ) {
-
   //If there are multiple strings in the bio, treat each as it's own paragraph
   const bioDetails : ReactElement[] = props.bio.map((statement) => {
     return <div key={uuidV4()}><ReactMarkdown>{statement}</ReactMarkdown></div>
   })
-  console.log('props.facebookLink:', props)
   return (
     <>
       <div className="guide--main-card">
         <div className="guide--main-text">
           <h1>{props.firstName} {props.lastName}</h1>
-          <h2>{props.firstName} is a {props.level} level Off-Roadeo guide in {props.location} {props.started && "and has been since "+props.started}</h2>
+          <h2>{props.firstName} is an Off-Roadeo guide in {props.location} {props.started && "and has been since "+props.started}</h2>
         </div>
         <div><img className="guide--headshot" src={props.headshot}></img></div>
         <div>{bioDetails}</div>
@@ -35,13 +33,7 @@ function Guide( props : guideDataInterface ) {
           {props.youTubeLink && <a href={props.youTubeLink}><img className="guide--social-logo" src={youTubeLogo}></img></a>}
           {props.linkedInLink && <a href={props.linkedInLink}><img className="guide--social-logo" src={linkedInLogo}></img></a>}
           {props.email && <a href={props.email}><img className="guide--social-logo" src={emailLogo}></img></a>}
-        </div>
-
-        <div>
-        {props.venmoLink && <a className="guide--tips-section" href={props.venmoLink}>
-          Buy {props.firstName} a cup of coffee
-          {props.venmoImage && <img className="guide--venmo-qr" src={props.venmoImage}></img>}
-          </a>}
+          {props.venmoLink && <a href={props.venmoLink}><img className="guide--social-logo" src={venmoLogo}></img></a>}
         </div>
       </div>
     </>
--- src/pages/Guides/GuideDetails/GuideDetailLoader.tsx
+++ src/pages/Guides/GuideDetails/GuideDetailLoader.tsx
@@ -6,17 +6,40 @@ async function GuideDetailLoader(req: LoaderFunctionArgs) {
   const { params } = req
   const { id } = params
   let guideId = 0
+  let guideName = ""
+  let guide: guideDataInterface
+  //If id exists
   if (id) {
     guideId = parseInt(id)
-  }
-  const guide : guideDataInterface = guideData.find(({ id }) => {
-    if (id === guideId) {
-      return true
+    //if the ID is a number
+    if (guideId) {
+      guide =
+        guideData.find(({ id }) => {
+          if (id === guideId) {
+            return true
+          }
+        }) || guideData[0]
+    } else {
+      //The id is a name
+      guideName = id
+      guide =
+        guideData.find(({ firstName }) => {
+          if (firstName === guideName) {
+            return true
+          }
+        }) || guideData[0]
     }
-  }) || guideData[0]
+  } else {
+    guide = guideData[0]
+  }
   if (guide) {
     return guide
   }
 }
+
+
+
+
+
 
 export default GuideDetailLoader
