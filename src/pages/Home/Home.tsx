54c79bdf9c9b7cde5d4259ec4edc6680c994b44c
Jason Martin <jasnmar@gmail.com>
2026-04-01 13:54:02 -0400

    starting

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
           <NavCard title="Offroading" link="offroading/offroad" altText="A bronco sport driving between some trees" image={offRoading}>Off-Roading, as we talk about it, is done mostly with **Broncos** and **Bronco Sports**.</NavCard>
           <NavCard title="Bronco Information" link="goatmodes" altText="A Red 2 door Bronco on some rocks" image={rockbronco}>Technical details related to Broncos.</NavCard>
         </div>
-        <p>This site is maintained by some of the guides at Bronco Off-Roadeo. It is not associated with or endorsed by <a href="http://www.ford.com" target="_blank">Ford</a> or <a href="http://www.broncooffroadeo.com" target="_blank">Bronco Off-Roadeo</a>.</p>
+        <p>This site is maintained by some of the guides at Bronco Off-Roadeo. It is not associated with or endorsed by <a href="http://www.ford.com" target="_blank" rel="noopener noreferrer">Ford</a> or <a href="http://www.broncooffroadeo.com" target="_blank" rel="noopener noreferrer">Bronco Off-Roadeo</a>.</p>
 
 
       {/* <Link to="/guides" >
