7fd69e86bf51a05daba71354af94d9ed4ff3695c
Jason Martin <jasnmar@gmail.com>
2026-04-03 11:16:42 -0400

    Astro

---
+++ src/assets_old/images.ts
@@ -0,0 +1,75 @@
+export { default as stuckBronco } from "./StuckBronco.webp"
+export { default as offRoading } from "./offroading1.webp"
+export { default as rockbronco } from "./rocksbronco.webp"
+
+//Recovery
+export { default as hdRecoveryKit } from "./recovery/M-1820-FPORRHD.webp"
+export { default as warnSoftShackle } from "./recovery/WarnSoftShackle.webp"
+export { default as arbEssentialsKit } from "./recovery/ARBEssentialsKit.webp"
+export { default as arbSoftShackle } from "./recovery/ARBSoftShackle.webp"
+export { default as fordRecoveryBoards } from "./recovery/FordRecoveryBoards.webp"
+export { default as arbTreadPro } from "./recovery/ARBTreadPro.webp"
+export { default as shovel } from "./recovery/shovel.webp"
+export { default as recoveryStrap } from "./recovery/ARBRecoveryStrap.webp"
+export { default as gloves } from "./recovery/gloves.webp"
+export { default as mgloves } from "./recovery/mgloves.webp"
+export { default as recoveryRope } from "./recovery/RecoveryRope.webp"
+export { default as bowShackle } from "./recovery/bowShackle.webp"
+export { default as treeSaver } from "./recovery/treeSaver.webp"
+// export { default as recoveryBoards } from "./recovery/recovery-board.jpg"
+
+
+
+//Guides
+export { default as jasonBio } from "./guides/jason-bio.webp"
+export { default as anonymousBio } from "./guides/anonymous.jpeg"
+
+export { default as fbLogo } from "./guides/icons/facebook.svg"
+export { default as xLogo } from "./guides/icons/x.svg"
+export { default as instLogo } from "./guides/icons/instagram.svg"
+export { default as youTubeLogo } from "./guides/icons/youtube.svg"
+export { default as linkedInLogo } from "./guides/icons/linkedin.svg"
+export { default as emailLogo } from "./guides/icons/email.svg"
+export { default as venmoLogo } from "./guides/icons/venmo.svg"
+
+
+//Boards
+
+export { default as bronco } from "./boards/Bronco-bucking.svg"
+export { default as beadlock } from "./boards/Beadlock.svg"
+export { default as tpmswarning } from "./boards/TPMS_warning_icon.svg"
+export { default as broncoABC } from "./boards/BroncoABC.svg"
+export { default as powerTrain } from "./boards/PowerTrain.svg"
+export { default as stabarDisconnect } from "./boards/StayBarDisconnect.svg"
+export { default as FrontLockingDifferential } from "./boards/FrontLockingDifferential.svg"
+export { default as RearLockingDifferential } from "./boards/RearLockingDifferential.svg"
+export { default as TrailTurnAssist } from "./boards/TrailTurnAssist.svg"
+export { default as stop } from "./boards/stop.svg"
+export { default as ComeForward } from "./boards/ComeForward.svg"
+export { default as PassDrive } from "./boards/PassDrive.svg"
+export { default as SlowDown } from "./boards/SlowDown.svg"
+export { default as groundSchool } from "./boards/ground-school.webp"
+export { default as orientation } from "./boards/orientation.webp"
+export { default as softSand } from "./boards/softsand.webp"
+export { default as downhill } from "./boards/downhill.webp"
+export { default as hillclimb } from "./boards/hillclimb.webp"
+export { default as crossing } from "./boards/crossing.webp"
+export { default as rockcrawling } from "./boards/rockCrawl.webp"
+export { default as sidetilt } from "./boards/sideTilt.webp"
+export { default as articMounds } from "./boards/articulationMounds.png"
+export { default as waterFord } from "./boards/waterford.webp"
+export { default as advRock } from "./boards/advRockCrawl.webp"
+
+
+//Goat Modes
+export { default as ecoMode } from "./bronco/goatmodes/eco.jpeg"
+export { default as bajaMode } from "./bronco/goatmodes/baja.jpeg"
+export { default as mudRutsMode } from "./bronco/goatmodes/mudruts.jpeg"
+export { default as normalMode } from "./bronco/goatmodes/normal.jpeg"
+export { default as rockCrawlMode } from "./bronco/goatmodes/rockcrawl.jpeg"
+export { default as sandMode } from "./bronco/goatmodes/sand.jpeg"
+export { default as slipperyMode } from "./bronco/goatmodes/slippery.jpeg"
+export { default as sportMode } from "./bronco/goatmodes/sport.jpeg"
+
+
+
